a4106a8c92602550f0c7ac65bfb15e3afc650691
el2toro <vasea.badragan@gmail.com>
2025-01-23 11:24:59 +0100

    Add fluent validation pipeline

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,12 +1,35 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Parameter Name is required.");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Parameter Description is required.");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Parameter ImageFile is required.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Parameter Price should be greater than 0.");
+    }
+}
+internal class CreateProductCommandHandler
+    (IDocumentSession session, IValidator<CreateProductCommand> validator)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        var result = await validator.ValidateAsync(command);
+        var errors = result.Errors.Select(e => e.ErrorMessage).ToList();
+
+        if (errors.Any())
+        {
+            throw new ValidationException(errors.FirstOrDefault());
+        }
+
         var product = new Product
         {
             Name = command.Name,
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,6 @@
+using BuildingBlocks.Behaviors;
+using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
@@ -5,7 +8,12 @@ builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    // Configure Mediator pipeline to be trigered befor handler
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
