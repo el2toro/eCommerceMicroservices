ad54fc45cfcb88c6fb95aeb13262ad1626231eca
el2toro <vasea.badragan@gmail.com>
2025-01-23 19:05:01 +0100

    Define basket features and models

---
+++ src/Services/Basket/Basket.API/Models/SoppingCart.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.API.Models;
+
+public class SoppingCart
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(i => i.Price * i.Quantity);
+
+    public SoppingCart(string username)
+    {
+        UserName = username;
+    }
+
+    public SoppingCart()
+    {
+    }
+}
