a6e474f93d34591e34ad87ad0b70c7538e8a0d86
el2toro <vasea.badragan@gmail.com>
2025-01-26 17:33:09 +0100

    Configure sqlite

---
+++ src/Services/Discount/Discount.gRPC/Data/DiscountContext.cs
@@ -0,0 +1,13 @@
+﻿using Discount.gRPC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.gRPC.Data;
+
+public class DiscountContext : DbContext
+{
+    public DbSet<Coupon> Coupones { get; set; }
+
+    public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+    {
+    }
+}
