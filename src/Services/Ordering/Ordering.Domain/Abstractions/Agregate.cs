9ce9263a9ef0659112ecfd461ba1c7ffe50cd8c9
el2toro <vasea.badragan@gmail.com>
2025-01-31 14:04:51 +0100

    Add domain abstractions

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Agregate.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Ordering.Domain.Abstractions;
+
+public abstract class Agregate<TId> : Entity<TId>, IAgregate<TId>
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyList<IDomainEvent> DomainEvent => _domainEvents.AsReadOnly();
+
+    public void AddDomainEvents(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+    public IDomainEvent[] ClearDomainIvents()
+    {
+        IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+        _domainEvents.Clear();
+
+        return dequeuedEvents;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAgregate.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public interface IAgregate<T> : IAgregate, IEntity<T>
+{
+
+}
+
+public interface IAgregate
+{
+    IReadOnlyList<IDomainEvent> DomainEvent { get; }
+    IDomainEvent[] ClearDomainIvents();
+}
