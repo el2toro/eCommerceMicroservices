56603ca15ded725ff43f01dec429227843401a46
el2toro <vasea.badragan@gmail.com>
2025-02-01 20:57:27 +0100

    Implement CreateOrder endpoint

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,15 +1,20 @@
-﻿namespace Ordering.API;
+﻿using Carter;
+
+namespace Ordering.API;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApiService(this IServiceCollection services)
     {
+        //Add services to the container
+        services.AddCarter();
 
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
+        app.MapCarter();
         return app;
     }
 }
