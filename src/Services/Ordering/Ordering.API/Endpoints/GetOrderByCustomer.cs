290d5a1ecacc91e65201025c140dd71ac66efbc6
el2toro <vasea.badragan@gmail.com>
2025-02-01 21:29:48 +0100

    Implement endpoints

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrderByCustomer.cs
@@ -0,0 +1,26 @@
+﻿using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+namespace Ordering.API.Endpoints;
+
+public record GetOrderByCustomerResponse(IEnumerable<OrderDto> Orders);
+
+public class GetOrderByCustomer : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId}", async (Guid cusomerId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByCustomerQuery(cusomerId));
+
+            var response = result.Adapt<GetOrderByCustomerResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrderByCustomer")
+        .Produces<GetOrderByCustomerResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Order By Customer")
+        .WithDescription("Get Order By Customer");
+    }
+}
