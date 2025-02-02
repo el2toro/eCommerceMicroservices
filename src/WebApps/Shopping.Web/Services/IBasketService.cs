8b589eff213fe13ea9beee5cb77361f501df7985
el2toro <vasea.badragan@gmail.com>
2025-02-02 21:13:34 +0100

    Implement services

---
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Web.Services;
+
+public interface IBasketService
+{
+    [Get("/basket-service/basket/{userName}")]
+    Task<GetBasketResponse> GetBasket(string userName);
+
+    [Get("/basket-service/basket/{userName}")]
+    Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
+
+    [Get("/basket-service/basket/{userName}")]
+    Task<DeleteBasketResponse> DeleteBasket(string userName);
+
+    [Get("/basket-service/basket/checkout")]
+    Task<CheckoutBasketRequest> CheckoutBasket(CheckoutBasketRequest request);
+}
