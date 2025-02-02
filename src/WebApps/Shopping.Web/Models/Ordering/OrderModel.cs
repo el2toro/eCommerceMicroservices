ab4b05c72bd72d8e969a627d9ca3150ead965f1c
el2toro <vasea.badragan@gmail.com>
2025-02-02 20:38:40 +0100

    Create models

---
+++ src/WebApps/Shopping.Web/Models/Ordering/OrderModel.cs
@@ -0,0 +1,35 @@
+﻿namespace Shopping.Web.Models.Ordering;
+
+public record OrderModel(
+    Guid Id,
+    Guid CustometId,
+    string OrderName,
+    AddresModel ShippingAddress,
+    AddresModel BillingAddress,
+    PaymentModel Payment,
+    OrderStatus Status,
+    List<OrderItemModel> OrderItems
+    );
+
+public record AddresModel(
+    string FirstName,
+    string LastName,
+    string EmailAddress,
+    string AddressLine,
+    string Country,
+    string State,
+    string ZipCode);
+public record OrderItemModel(Guid OrderId, Guid PriductId, int Quantity, decimal Price);
+public record PaymentModel(string CardName, string CardNumber, string Expiration, string Cvv, int PaymentMethod);
+public enum OrderStatus
+{
+    Draft = 0,
+    Pending = 1,
+    Completed = 2,
+    Canceled = 3
+}
+
+//Wrappers
+public record GetOrdersResponse(PaginatedResult<OrderModel> Orders);
+public record GetOrdersByNameResponse(IEnumerable<OrderModel> Orders);
+public record GetOrdersByCustomerResponse(IEnumerable<OrderModel> Orders);
