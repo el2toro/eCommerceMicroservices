5d1c1ba0ab85182fe31cd525d8cd99cd3f4933ee
el2toro <vasea.badragan@gmail.com>
2025-02-02 13:51:02 +0100

    Implement RabbitMq MassTransit

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -0,0 +1,35 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using static MassTransit.MessageHeaders;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extentions
+{
+    public static IServiceCollection AddMessageBroker
+        (this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+    {
+        //Implement RabbitMQ MassTransit Configuration
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly is not null)
+                config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]!);
+                    host.Password(configuration["MessageBroker:Password"]!);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
