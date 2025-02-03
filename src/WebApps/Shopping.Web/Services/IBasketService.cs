9a72165bc85b3a3efb6fa597cc790904429595c3
el2toro <vasea.badragan@gmail.com>
2025-02-03 15:54:53 +0100

    Fix basket endpoint

--- src/WebApps/Shopping.Web/Services/IBasketService.cs
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -7,13 +7,13 @@ public interface IBasketService
     [Get("/basket-service/basket/{userName}")]
     Task<GetBasketResponse> GetBasket(string userName);
 
-    [Get("/basket-service/basket/{userName}")]
+    [Post("/basket-service/basket")]
     Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
 
     [Get("/basket-service/basket/{userName}")]
     Task<DeleteBasketResponse> DeleteBasket(string userName);
 
-    [Get("/basket-service/basket/checkout")]
+    [Post("/basket-service/basket/checkout")]
     Task<CheckoutBasketRequest> CheckoutBasket(CheckoutBasketRequest request);
 
 
