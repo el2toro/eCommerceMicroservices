2b62b4e053303719880df9495e866ee6e711f062
el2toro <vasea.badragan@gmail.com>
2025-02-03 11:42:02 +0100

    Implement Cart page

--- src/WebApps/Shopping.Web/Services/IBasketService.cs
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -1,4 +1,6 @@
-﻿namespace Shopping.Web.Services;
+﻿using System.Net;
+
+namespace Shopping.Web.Services;
 
 public interface IBasketService
 {
@@ -13,4 +15,28 @@ public interface IBasketService
 
     [Get("/basket-service/basket/checkout")]
     Task<CheckoutBasketRequest> CheckoutBasket(CheckoutBasketRequest request);
+
+
+    public async Task<ShoppingCartModel> LoadUserBasket()
+    {
+        //Get basket if bot exist, create new basket with defalt logged in user name: swn
+        var userName = "swn";
+        ShoppingCartModel basket;
+
+        try
+        {
+            var getBasketResponse = await GetBasket(userName);
+            basket = getBasketResponse.Cart;
+        }
+        catch (ApiException apiException) when (apiException.StatusCode == HttpStatusCode.NotFound)
+        {
+            basket = new ShoppingCartModel
+            {
+                UserName = userName,
+                Items = []
+            };
+        }
+
+        return basket;
+    }
 }
